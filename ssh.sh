5bff094c1438e028a7c19e6d20a10e618153edb4
BALAJI POTHULA <balan.pothula@gmail.com>
2019-07-01 11:31:02 +0530

    Update ssh.sh

--- ssh.sh
+++ ssh.sh
@@ -7,9 +7,8 @@
 # ssh-keygen -q -N '' -m pem -t rsa -b 4096 -C balaji.pothula@techie.com -f `pwd`/webapp && mv `pwd`/webapp `pwd`/webapp.pem && chmod 400 `pwd`/webapp.pem
 
 readonly USR=ubuntu
-readonly HOST=13.126.42.22 #ec2-13-126-42-22.ap-south-1.compute.amazonaws.com
+readonly HOST=1.2.3.4 #ec2-1-2-3-4.ap-south-1.compute.amazonaws.com
 readonly PORT=22
 readonly PEM=webapp.pem
 
 ssh -o ServerAliveInterval=60 -o ServerAliveCountMax=30 -o CheckHostIP=no -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null -i `pwd`/$PEM $USR@$HOST -p $PORT
-
