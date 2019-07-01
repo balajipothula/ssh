83ba93b4ef3883c023c2859271aa932673f5fa98
BALAJI POTHULA <balan.pothula@gmail.com>
2019-07-01 11:29:39 +0530

    Update gen-cert.sh

--- gen-cert.sh
+++ gen-cert.sh
@@ -5,48 +5,19 @@
 # Description : Generating Let's Encrypt SSL Certs using certbot client.
 
 # Generating Diffie-Hellman pem file.
-openssl dhparam -out /home/ubuntu/webapp/vashs.in/ssl/dh.pem                   2048
-openssl dhparam -out /home/ubuntu/webapp/gmtauto.in/ssl/dh.pem                 2048
-openssl dhparam -out /home/ubuntu/webapp/joycatering.in/ssl/dh.pem             2048
-openssl dhparam -out /home/ubuntu/webapp/geo.balaji.network/ssl/dh.pem         2048
-openssl dhparam -out /home/ubuntu/webapp/2048.balaji.network/ssl/dh.pem        2048
-openssl dhparam -out /home/ubuntu/webapp/bingo.balaji.network/ssl/dh.pem       2048
-openssl dhparam -out /home/ubuntu/webapp/rubiks.balaji.network/ssl/dh.pem      2048
-openssl dhparam -out /home/ubuntu/webapp/bsc.results.balaji.network/ssl/dh.pem 2048
+openssl dhparam -out /home/ubuntu/webapp/balaji.network/ssl/dh.pem 2048
 
 # Generating LetsEncrypt Certs.
-certbot certonly --standalone -q -n -d vashs.in             -d www.vashs.in       -m balan.pothula@gmail.com --agree-tos --no-eff-email
-certbot certonly --standalone -q -n -d gmtauto.in           -d www.gmtauto.in     -m balan.pothula@gmail.com --agree-tos --no-eff-email
-certbot certonly --standalone -q -n -d joycatering.in       -d www.joycatering.in -m balan.pothula@gmail.com --agree-tos --no-eff-email
-certbot certonly --standalone -q -n -d geo.balaji.network                         -m balan.pothula@gmail.com --agree-tos --no-eff-email
-certbot certonly --standalone -q -n -d 2048.balaji.network                        -m balan.pothula@gmail.com --agree-tos --no-eff-email
-certbot certonly --standalone -q -n -d bingo.balaji.network                       -m balan.pothula@gmail.com --agree-tos --no-eff-email
-certbot certonly --standalone -q -n -d rubiks.balaji.network                      -m balan.pothula@gmail.com --agree-tos --no-eff-email
-certbot certonly --standalone -q -n -d bsc.results.balaji.network                 -m balan.pothula@gmail.com --agree-tos --no-eff-email
+certbot certonly --standalone -q -n -d balaji.network -d www.balaji.network -m balan.pothula@gmail.com --agree-tos --no-eff-email
 
 # Copying letsencrypt certs into volume.
-cp -f /etc/letsencrypt/live/vashs.in/{cert.pem,privkey.pem,chain.pem}                   /home/ubuntu/webapp/vashs.in/ssl
-cp -f /etc/letsencrypt/live/gmtauto.in/{cert.pem,privkey.pem,chain.pem}                 /home/ubuntu/webapp/gmtauto.in/ssl
-cp -f /etc/letsencrypt/live/joycatering.in/{cert.pem,privkey.pem,chain.pem}             /home/ubuntu/webapp/joycatering.in/ssl
-cp -f /etc/letsencrypt/live/geo.balaji.network/{cert.pem,privkey.pem,chain.pem}         /home/ubuntu/webapp/geo.balaji.network/ssl
-cp -f /etc/letsencrypt/live/2048.balaji.network/{cert.pem,privkey.pem,chain.pem}        /home/ubuntu/webapp/2048.balaji.network/ssl
-cp -f /etc/letsencrypt/live/bingo.balaji.network/{cert.pem,privkey.pem,chain.pem}       /home/ubuntu/webapp/bingo.balaji.network/ssl
-cp -f /etc/letsencrypt/live/rubiks.balaji.network/{cert.pem,privkey.pem,chain.pem}      /home/ubuntu/webapp/rubiks.balaji.network/ssl
-cp -f /etc/letsencrypt/live/bsc.results.balaji.network/{cert.pem,privkey.pem,chain.pem} /home/ubuntu/webapp/bsc.results.balaji.network/ssl
+cp -f /etc/letsencrypt/live/balaji.network/{cert.pem,privkey.pem,chain.pem} /home/ubuntu/webapp/balaji.network/ssl
 
 # Changing Certs Permission to -rw-r--r--.
-chmod 644 /home/ubuntu/webapp/vashs.in/ssl/*.pem
-chmod 644 /home/ubuntu/webapp/gmtauto.in/ssl/*.pem
-chmod 644 /home/ubuntu/webapp/joycatering.in/ssl/*.pem
-chmod 644 /home/ubuntu/webapp/geo.balaji.network/ssl/*.pem
-chmod 644 /home/ubuntu/webapp/2048.balaji.network/ssl/*.pem
-chmod 644 /home/ubuntu/webapp/bingo.balaji.network/ssl/*.pem
-chmod 644 /home/ubuntu/webapp/rubiks.balaji.network/ssl/*.pem
-chmod 644 /home/ubuntu/webapp/bsc.results.balaji.network/ssl/*.pem
+chmod 644 /home/ubuntu/webapp/balaji.network/ssl/*.pem
 
 # Creating Symbolic Link.
-ln -s -f /etc/letsencrypt/live/vashs.in/cert.pem      /home/ubuntu/webapp/vashs.in/ssl/cert.pem
-ln -s -f /etc/letsencrypt/live/vashs.in/privkey.pem   /home/ubuntu/webapp/vashs.in/ssl/privkey.pem
-ln -s -f /etc/letsencrypt/live/vashs.in/chain.pem     /home/ubuntu/webapp/vashs.in/ssl/chain.pem
-ln -s -f /etc/letsencrypt/live/vashs.in/fullchain.pem /home/ubuntu/webapp/vashs.in/ssl/fullchain.pem
-
+ln -s -f /etc/letsencrypt/live/balaji.network/cert.pem      /home/ubuntu/webapp/balaji.network/ssl/cert.pem
+ln -s -f /etc/letsencrypt/live/balaji.network/privkey.pem   /home/ubuntu/webapp/balaji.network/ssl/privkey.pem
+ln -s -f /etc/letsencrypt/live/balaji.network/chain.pem     /home/ubuntu/webapp/balaji.network/ssl/chain.pem
+ln -s -f /etc/letsencrypt/live/balaji.network/fullchain.pem /home/ubuntu/webapp/balaji.network/ssl/fullchain.pem
