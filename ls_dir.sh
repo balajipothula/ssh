bc5f0ff37be8f07cb067238138282f85e628e1ce
BALAJI POTHULA <balan.pothula@gmail.com>
2019-08-21 22:23:13 +0530

    Create ls_dir.sh

---
+++ ls_dir.sh
@@ -0,0 +1,44 @@
+#!/bin/bash
+
+# run ./ls_dir.sh or bash ls_dir.sh only not sh ls_dir.sh
+
+dirs=$(ls -l --time-style='long-iso' $HOME | egrep '^d' | awk '{print $8}')
+
+for dir in $dirs; do
+  echo $dir
+done
+
+
+dir_array=(`ls`)
+
+dir_array_size=${#dir_array[*]}
+
+i=0
+while [ $i -lt $dir_array_size ]; do
+  if [ -d "${dir_array[$i]}" ]; then
+    echo "${dir_array[$i]}"
+  fi
+  let i++
+done
+
+#!/bin/bash
+
+Dockerfile="$HOME/Dockerfile"
+while IFS= read -r line; do
+  echo "$line"
+done < $Dockerfile
+
+
+arr=()
+arr+=('a')
+arr+=('b')
+arr+=('c')
+
+SEARCH_STRING='b'
+
+if [[ " ${arr[*]} " == *"$SEARCH_STRING"* ]];
+then
+    echo "YES, your arr contains $SEARCH_STRING"
+else
+    echo "NO, your arr does not contain $SEARCH_STRING"
+fi
