48c3ae74da404cfc89939bce8c40b88a9fc4c6a6
BALAJI POTHULA <balan.pothula@gmail.com>
2019-09-02 18:49:26 +0530

    Update ssh-help.sh

--- ssh-help.sh
+++ ssh-help.sh
@@ -9,3 +9,9 @@ ssh -o CheckHostIP=no -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/nul
 
 # Checking python version remotely using ssh.
 ssh -o CheckHostIP=no -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null -i $HOME/.ssh/id_rsa ec2-user@10.0.0.11 python -V
+
+# downloading oracle jdk.
+curl -JL -b "oraclelicense=a" https://download.oracle.com/otn/java/jdk/8u221-b11/230deb18db3e4014bb8e3e8324f81b43/jre-8u221-linux-x64.tar.gz?AuthParam=1567421871_fb2f3fff5a0da2ba2d1a38aeda898a29 -o jdk.tar.gz
+
+# downloading oracle jre.
+curl -JL -b "oraclelicense=a" https://download.oracle.com/otn/java/jdk/8u221-b11/230deb18db3e4014bb8e3e8324f81b43/jre-8u221-linux-x64.tar.gz?AuthParam=1567421871_fb2f3fff5a0da2ba2d1a38aeda898a29 -o jre.tar.gz
