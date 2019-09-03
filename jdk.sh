09e15586868488d30dfecdb3070ccf6fc7d6cdd2
BALAJI POTHULA <balan.pothula@gmail.com>
2019-09-03 13:26:10 +0530

    Update jdk.sh

--- jdk.sh
+++ jdk.sh
@@ -2,7 +2,7 @@ curl -JL https://github.com/frekele/oracle-java/releases/download/8u212-b10/jdk-
 tar -xzf $HOME/jdk.tar.gz -C $HOME                                                                                         && \
 rm  -rf  $HOME/jdk.tar.gz                                                                                                  && \
 mv       $HOME/jdk* $HOME/jreroot                                                                                          && \
-echo "export PATH=$PATH:$HOME/jdkroot/bin" >> $HOME/.bashrc                                                                && \
+echo "export     PATH=$PATH:$HOME/jdkroot/bin" >> $HOME/.bashrc                                                            && \
 echo "export JDK_HOME=$PATH:$HOME/jdkroot/bin" >> $HOME/.bashrc                                                            && \
 echo "export JRE_HOME=$PATH:$HOME/jdkroot/jre" >> $HOME/.bashrc                                                            && \
 find     $HOME/jdkroot -maxdepth 2 -name "*.txt"  -type f -delete                                                          && \
